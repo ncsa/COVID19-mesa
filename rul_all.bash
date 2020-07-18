f369bf4695dee7aa94f7c2d96c1bc079a711535b
Santiago Nunez-Corrales <snunezcr@gmail.com>
2020-07-18 17:39:47 -0500

    Master run script

---
+++ rul_all.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+python model_runner.py 30 scenarios scenarios/cu-25-ntrace-nisol.json
+python model_runner.py 30 scenarios scenarios/cu-25-ntrace-yisol.json
+python model_runner.py 30 scenarios scenarios/cu-25-ytrace-nisol.json
+python model_runner.py 30 scenarios scenarios/cu-25-ytrace-yisol.json
+python model_runner.py 30 scenarios scenarios/cu-50-ntrace-nisol.json
+python model_runner.py 30 scenarios scenarios/cu-50-ntrace-yisol.json
+python model_runner.py 30 scenarios scenarios/cu-50-ytrace-nisol.json
+python model_runner.py 30 scenarios scenarios/cu-50-ytrace-yisol.json
+python model_runner.py 30 scenarios scenarios/cu-75-ntrace-nisol.json
+python model_runner.py 30 scenarios scenarios/cu-75-ntrace-yisol.json
+python model_runner.py 30 scenarios scenarios/cu-75-ytrace-nisol.json
+python model_runner.py 30 scenarios scenarios/cu-75-ytrace-yisol.json
