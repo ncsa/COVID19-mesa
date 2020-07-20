fad0d4cf8d0c4a5e70b8378ce4aa17b4cc72d5a7
Santiago Nunez-Corrales <snunezcr@gmail.com>
2020-07-20 18:23:21 -0500

    Fixing all runner

--- run_all.bash
+++ run_all.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-python model_runner.py 30 scenarios scenarios/cu-25-nisol.json
-python model_runner.py 30 scenarios scenarios/cu-25-yisol.json
-python model_runner.py 30 scenarios scenarios/cu-50-nisol.json
-python model_runner.py 30 scenarios scenarios/cu-50-yisol.json
-python model_runner.py 30 scenarios scenarios/cu-75-nisol.json
-python model_runner.py 30 scenarios scenarios/cu-75-yisol.json
-python model_runner.py 30 scenarios scenarios/cu-counterfactual.json
+python model_runner.py 30 scenarios/cu-25-nisol.json
+python model_runner.py 30 scenarios/cu-25-yisol.json
+python model_runner.py 30 scenarios/cu-50-nisol.json
+python model_runner.py 30 scenarios/cu-50-yisol.json
+python model_runner.py 30 scenarios/cu-75-nisol.json
+python model_runner.py 30 scenarios/cu-75-yisol.json
+python model_runner.py 30 scenarios/cu-counterfactual.json
