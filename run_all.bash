4e4d99e938068265e9e0e0a93705f9fedd3ea6c3
Santiago Nunez-Corrales <snunezcr@gmail.com>
2020-07-21 11:54:32 -0500

    Further calibration changes

--- run_all.bash
+++ run_all.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+python model_runner.py 30 scenarios/cu-calibration.json
 python model_runner.py 30 scenarios/cu-25-nisol.json
 python model_runner.py 30 scenarios/cu-25-yisol.json
 python model_runner.py 30 scenarios/cu-50-nisol.json
