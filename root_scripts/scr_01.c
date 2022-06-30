eed0634a8bab6cf97f99aa1a27bb1cfb2756b561
Xinyi Huang <xinyih8@illinois.edu>
2022-06-30 10:25:04 -0500

    average

--- root_scripts/scr_01.c
+++ root_scripts/scr_01.c
@@ -1,14 +1,17 @@
 void src_01() {
     auto d = ROOT::RDF::MakeCsvDataFrame("data.csv");
-    auto maxStep = d.Max<Int_t>("Step");
+    auto maxStep = d.Max("Step");
 
-    for (Int_t i = 0; i < maxStep; i++) {
+    std::vector<Int_t> stepno_;
+    std::vector<Double_t> average_;
+
+    for (Int_t i = 0; i <= maxStep; i++) {
         stepno_.push_back(i);
         average_.push_back(d.Filter("Step == i").Mean<Double_t>("Susceptible"));
     }
 
-    std::function<const Double_t&(Double_t)> ave = [&average_](Double_t i) -> const Double_t& { return average_[i];};
-    std::function<const Int_t&(Int_t)> ste = [&stepno_](Int_t i) -> const Int_t& { return stepno_[i];};
+    std::function<Double_t&(Double_t)> ave = [&average_](Double_t i) -> Double_t& { return average_[i];};
+    std::function<Int_t&(Int_t)> ste = [&stepno_](Int_t i) -> Int_t& { return stepno_[i];};
     d.Define("average", ave);
     d.Define("stepno", ste);
     auto myGraph1 = d.Graph<Int_t, Double_t>("stepno", "average");
