fcdafa3bdf26587a37bb9bdfd7781c179e4e2ebf
Xinyi Huang <xinyih8@illinois.edu>
2022-07-08 23:09:02 -0500

    average

--- root_scripts/src_01.c
+++ root_scripts/src_01.c
@@ -6,25 +6,29 @@ void src_01() {
        TFile::Cp(fileNameUrl, fileName);
     auto d = ROOT::RDF::MakeCsvDataFrame(fileName);
 
-    auto h = d.Histo2D({"h2", "ptD0 vs Dm_d", 30, 0.135, 0.165, 30, -3, 6}, "Step", "Susceptible");
-    h->Draw();
+    // auto h = d.Histo2D({"h2", "ptD0 vs Dm_d", 30, 0.135, 0.165, 30, -3, 6}, "Step", "Susceptible");
+    // h->Draw();
 
-    // auto maxStep = d.Max("Step");
+    auto maxStep = d.Max("Step");
 
-    // std::vector<Int_t> stepno_;
-    // std::vector<Double_t> average_;
+    std::vector<Int_t> stepno_;
+    std::vector<Double_t> average_;
 
-    // for (Int_t i = 0; i <= maxStep; i++) {
-    //     stepno_.push_back(i);
-    //     auto a = d.Filter("Step == i").Mean("Susceptible");
-    //     average_.push_back(a);
-    // }
+    for (Int_t i = 0; i <= maxStep; i++) {
+        stepno_.push_back(i);
+        auto a = d.Filter("Step == i").Mean("Susceptible");
+        average_.push_back(a);
+    }
 
 
-    // std::function<Double_t&(Double_t)> ave = [&average_](Double_t i) -> Double_t& { return average_[i];};
-    // std::function<Int_t&(Int_t)> ste = [&stepno_](Int_t i) -> Int_t& { return stepno_[i];};
-    // d.Define("average", ave);
-    // d.Define("stepno", ste);
-    // auto myGraph1 = d.Graph<Int_t, Double_t>("stepno", "average");
-    // myGraph1->Draw();
+    std::function<Double_t&(Double_t)> ave = [&average_](Double_t i) -> Double_t& { return average_[i];};
+    std::function<Int_t&(Int_t)> ste = [&stepno_](Int_t i) -> Int_t& { return stepno_[i];};
+    auto d2 = d.Define("average", ave);
+               .Define("stepno", ste);
+    auto myGraph1 = d2.Histo2D("stepno", "average");
+
+    auto c = new TCanvas();
+    c->SetLogx();
+    c->SetLogy();
+    myGraph1->DrawClone();
 }
